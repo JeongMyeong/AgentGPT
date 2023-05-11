e2d8338d8c97f3d6156fe93bc9aab1a9b7b4ece7
JEONGMYEONG CHOI <44393016+JeongMyeong@users.noreply.github.com>
2023-05-11 14:54:23 +0900

    Update setup.sh
    This pull request resolves Issue #508.
    
    The issue occurred when running setup.sh, and the following error was encountered:
    
    
    ❌ Invalid environment variables:
     NEXT_PUBLIC_BACKEND_URL: Required
    
    To fix this issue, I added the missing NEXT_PUBLIC_BACKEND_URL variable to the environment (ENV) in ./setup.sh.
    
    Please review the changes and let me know if any further modifications are needed.

--- setup.sh
+++ setup.sh
@@ -23,7 +23,9 @@ ENV="NODE_ENV=development\n\
 NEXTAUTH_SECRET=$NEXTAUTH_SECRET\n\
 NEXTAUTH_URL=http://localhost:3000\n\
 OPENAI_API_KEY=$OPENAI_API_KEY\n\
-DATABASE_URL=file:../db/db.sqlite\n"
+DATABASE_URL=file:../db/db.sqlite\n\
+NEXT_PUBLIC_BACKEND_URL=http://localhost:3000\n"
+
 
 cd next
 printf $ENV > .env
